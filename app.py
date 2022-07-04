3f27e453c20de839bf6eed00e1aedcc70a1b7707
Diana Huang <dianahuang@Dianas-MBP.lan>
2022-07-03 19:44:35 -1000

    finished can't get flash to work properly

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from urllib import response
 from flask import Flask, request, render_template, redirect, flash
 
 import surveys
@@ -11,6 +13,7 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
+questions = surveys.satisfaction_survey.questions
 
 @app.route('/')
 def show_home_page():
@@ -23,6 +26,26 @@ def show_home_page():
 
 @app.route('/questions/<int:num>')
 def show_questions(num):
-    questions = surveys.satisfaction_survey.questions
-    # questions_length = len(questions) -1
-    return render_template('questions.html', questions=questions, num=num)
+    if num != len(responses):
+        if num > len(responses):
+            flash('please answer the questions in order', 'error')
+            return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(responses)}")
+    
+    elif num == len(questions):
+        return redirect('/thankyou')
+    else:
+        return render_template('questions.html', questions=questions, num=num)
+
+@app.route('/answer/<int:num>', methods=['POST'])
+def test(num):
+    if len(responses) < len(questions):
+        option = request.form['options']
+        responses.append(option)
+        return redirect(f"/questions/{num}")
+    else:
+        return redirect('/thankyou')
+    
+@app.route('/thankyou')
+def thank_you():
+    return render_template('thankyou.html', responses=responses)
